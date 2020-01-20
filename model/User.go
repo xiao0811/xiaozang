551336030170e8b4979b9b5e2a3a12acb2b87cfb
xiaosha <panxiao0811@gmail.com>
2020-01-20 15:20:15 +0800

    create

--- model/User.go
+++ model/User.go
@@ -1,19 +1,16 @@
 package model
 
 import (
-	"time"
+	"github.com/jinzhu/gorm"
 )
 
 // User Model
 type User struct {
-	ID         uint       `gorm:"primary_key" json:"id"`
-	CreatedAt  time.Time  `json:"created_at"`
-	UpdatedAt  time.Time  `json:"updated_at"`
-	DeletedAt  *time.Time `sql:"index" json:"deleted_at"`
-	Username   string     `json:"username" form:"username"`
-	Password   string     `json:"password" form:"password"`
-	Department uint8      `json:"department" form:"department"`
-	Email      string     `json:"email" form:"email"`
+	gorm.Model
+	Username   string `json:"username" form:"username"`
+	Password   string `json:"password" form:"password"`
+	Department uint8  `json:"department" form:"department"`
+	Email      string `json:"email" form:"email"`
 }
 
 // TableName 重新设置表名
